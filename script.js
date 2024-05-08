442a960e7f7db32dcda82aae3212db2397984ab3
mohammed kaif <mkaif555123@gmail.com>
2024-05-09 00:37:58 +0500

    90% work done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// function clk(e) {
-//     document.getElementById("textPerform").innerHTML += e
-// }
-// function clr() {
-//     document.getElementById("textPerform").innerHTML = " "
-// }
-// function eql() {
-//     let a = document.getElementById("textPerform")
-//     a.innerHTML = eval(a.innerHTML)
-// }
+function clk(e) {
+    document.getElementById("textPerform").innerHTML += e
+}
+function clr() {
+    document.getElementById("textPerform").innerHTML = " "
+}
+function eql() {
+    let a = document.getElementById("textPerform")
+    a.innerHTML = eval(a.innerHTML)
+}
