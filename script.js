eab497560b0a085dc5f7f664611070f64b1de06c
mohammed kaif <mkaif555123@gmail.com>
2024-05-09 00:38:58 +0500

    95% work done

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function clk(e) {
     document.getElementById("textPerform").innerHTML += e
 }
+
+
 function clr() {
     document.getElementById("textPerform").innerHTML = " "
+
 }
+
 function eql() {
     let a = document.getElementById("textPerform")
     a.innerHTML = eval(a.innerHTML)
