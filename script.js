dda281a75f4aea0167fe49d05ad78fa6543775d4
mohammed kaif <mkaif555123@gmail.com>
2024-05-09 00:41:04 +0500

    100% WORK DONE

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function clk(e) {
   document.getElementById("textPerform").innerHTML += e;
 }
-
 function clr() {
   document.getElementById("textPerform").innerHTML = " ";
 }
-
 function eql() {
   let a = document.getElementById("textPerform");
   a.innerHTML = eval(a.innerHTML);
